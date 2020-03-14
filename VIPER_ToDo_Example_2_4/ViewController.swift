0acae97d2fe74c9cddc80d1e4acf62560b355b19
laurayesayan <laurayesayan@gmail.com>
2020-03-14 17:01:06 +0300

    Initial Commit

---
+++ VIPER_ToDo_Example_2_4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER_ToDo_Example_2_4
+//
+//  Created by Лаура Есаян on 14.03.2020.
+//  Copyright © 2020 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
